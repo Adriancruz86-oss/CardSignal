2b506d65d5b52309dd2cba767df5813aa232da7e
Adriancruz86-oss <Adrian_cruz86@icloud.com>
2026-09-04 10:13:57 -0400

    Mount CardSignal functional interaction layer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import FunctionalLayer from "./functional-layer";
 import "./globals.css";
 import "./asset-overrides.css";
 
@@ -24,7 +25,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <FunctionalLayer />
+      </body>
     </html>
   );
 }
