c58fcc28c9a8a2621e0a0b5b36478d4e4ed46609
Adriancruz86-oss <Adrian_cruz86@icloud.com>
2026-09-04 13:02:35 -0400

    Mount persistent user card layer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import FunctionalLayer from "./functional-layer";
 import CardDetailLayer from "./card-detail-layer";
+import UserCardLayer from "./user-card-layer";
 import "./globals.css";
 import "./asset-overrides.css";
 
@@ -30,6 +31,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
         {children}
         <FunctionalLayer />
         <CardDetailLayer />
+        <UserCardLayer />
       </body>
     </html>
   );
