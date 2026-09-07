9c0ec608514746d24c5a2036f8214808ed0c1831
Adriancruz86-oss <Adrian_cruz86@icloud.com>
2026-09-07 18:56:38 -0400

    Refactor sealed layout for unified dashboard

--- src/app/sealed/layout.tsx
+++ src/app/sealed/layout.tsx
@@ -1,17 +1,5 @@
 import type { ReactNode } from "react";
-import LiveSealedDiscovery from "./live-sealed-discovery";
-import HotRightNow from "./hot-right-now";
-import SealedOwnership from "./sealed-ownership";
-import SealedVisualShelf from "./sealed-visual-shelf";
 
 export default function SealedLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <LiveSealedDiscovery />
-      <HotRightNow />
-      <SealedOwnership />
-      <SealedVisualShelf />
-      {children}
-    </>
-  );
+  return children;
 }
